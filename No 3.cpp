87c392da4255c95a6ce01788998cb7ba86b5085f
BhanuAzizi <114640944+BhanuAzizi@users.noreply.github.com>
2022-10-30 20:59:47 +0700

    Update No 3.cpp

--- No 3.cpp
+++ No 3.cpp
@@ -9,13 +9,9 @@ int main()
 	cin >> bil;
 	cout << endl;
 
-	if (bil%400==0){
+	if (bil%4==0){
 		cout << bil << " Adalah Tahun Kabisat"<<endl;
-    }else if (bil%100==0){
-        cout << bil << " Bukan Tahun Kabisat" << endl;
-    }else if (bil%4==0){
-        cout << bil << " Adalah Tahun Kabisat" << endl;
-	}else {
+    }else {
 		cout<<bil<<" Adalah Bukan Tahun Kabisat"<<endl;
 	}
 	return 0;
